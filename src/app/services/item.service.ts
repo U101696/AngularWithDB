d52928669763a9c243d39f3b6e815836c39e7425
Lyka Go <lyka.go@pulsar-sd.com>
2018-06-08 16:55:41 +0800

    first commit

---
+++ src/app/services/item.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { Item } from '../models/Item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+  itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+
+  constructor(public afs: AngularFirestore) { 
+    this.items = this.afs.collection('items').valueChanges();
+  }
+
+  getItem(){
+    return this.items;
+  }
+}
+
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDJYYTepBhWAHZWmKFNxzLT4pXHv9efQKY",
+    authDomain: "fs1prod-6fb09.firebaseapp.com",
+    databaseURL: "https://fs1prod-6fb09.firebaseio.com",
+    projectId: "fs1prod-6fb09",
+    storageBucket: "fs1prod-6fb09.appspot.com",
+    messagingSenderId: "445782923897"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
