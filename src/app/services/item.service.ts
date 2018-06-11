1524fd15df3f61273f64f9a3b64ede1a71223f81
Lyka Go <lyka.go@pulsar-sd.com>
2018-06-11 15:08:37 +0800

    June 11 2018

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import { Item } from '../models/Item';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,23 @@ export class ItemService {
   items: Observable<Item[]>;
 
   constructor(public afs: AngularFirestore) { 
-    this.items = this.afs.collection('items').valueChanges();
+    //this.items = this.afs.collection('items').valueChanges();
+    this.itemsCollection = this.afs.collection('items');
+    this.items = this.itemsCollection.snapshotChanges().map(changes => {
+      return changes.map(a => {
+        const data = a.payload.doc.data() as Item;
+        data.id = a.payload.doc.id;
+        return data;
+      });
+    });
   }
 
   getItem(){
     return this.items;
+  }
+
+  addItem(item: Item){
+    this.itemsCollection.add(item);
   }
 }
 
